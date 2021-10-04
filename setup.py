b6800ebf29887b454d8cf6004376d898ddc53b31
qx6145 <loic.balland@engie.com>
2021-10-04 17:45:48 +0200

    requirements for heroku

--- setup.py
+++ setup.py
@@ -10,21 +10,24 @@ setup(
     version='1.0',
     packages=['pm_utils', 'open_saturn'],
     package_dir={'pm_utils': 'pm_utils', 'open_saturn': 'open_saturn'},
+    package_data={'open_saturn': [
+        'templates/*'
+    ]},
     install_requires=[
-        'gunicorn',
-        'psycopg2',
-        'flask<2.0',
-        'tshistory_refinery @ https://hg.sr.ht/~pythonian/tshistory_refinery/archive/0.2.0.tar.gz#egg=tshistory_refinery',
-        'tzlocal~=2.0',
-        'werkzeug',
+        'Flask',
+        'pandas >= 1.0',
         'mock',
         'inireader',
         'apscheduler',
+        'tshistory_editor @ https://hg.sr.ht/~pythonian/tshistory_editor/archive/0.8.2.tar.gz#egg=tshistory_editor',
+        'tshistory_rest @ https://hg.sr.ht/~pythonian/tshistory_rest/archive/0.10.0.tar.gz#egg=tshistory_rest',
+        'tshistory_refinery @ https://hg.sr.ht/~pythonian/tshistory_refinery/archive/0.2.0.tar.gz#egg=tshistory_refinery',
     ],
     entry_points={
         'tshistory.subcommands': [
             'openwebstart=open_saturn.cli:openwebstart',
 
         ],
-    }
+    },
+    extras_require={'heroku': ["gunicorn", "psycopg2"]},
 )
