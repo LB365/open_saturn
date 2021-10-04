f61a062868416133f138263d489d11f7cd6bf8ee
qx6145 <loic.balland@engie.com>
2021-10-04 11:46:53 +0200

    tshisto_ref in setuptools

--- Procfile
+++ Procfile
@@ -2,4 +2,4 @@ worker: pip install https://hg.sr.ht/~pythonian/tshistory_refinery/archive/0.2.0
 worker: tsh init-db DATABASE_URL
 worker: tsh formula-init-db DATABASE_URL
 worker: rework init-db DATABASE_URL
-web: gunicorn open_saturn.wsgi:heroku_app
+web: gunicorn open_saturn.wsgi: heroku_app
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='open_saturn',
-    version='2.0',
+    version='1.0',
     packages=['pm_utils', 'open_saturn'],
     package_dir={'pm_utils': 'pm_utils', 'open_saturn': 'open_saturn'},
     install_requires=[
         'gunicorn',
         'psycopg2',
         'flask<2.0',
+        'tshistory_refinery @ https://hg.sr.ht/~pythonian/tshistory_refinery/archive/0.2.0.tar.gz`'
         'tzlocal~=2.0',
         'werkzeug',
         'mock',
