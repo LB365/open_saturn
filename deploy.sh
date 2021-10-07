88243dbbb34fa67233041cf39e69c935dcfabc57
qx6145 <loic.balland@engie.com>
2021-10-07 22:08:13 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-export INSTALL_ON_LINUX=1;pip install -e.
-tsh init-db DATABASE_URL
-tsh formula-init-db DATABASE_URL
-rework init-db DATABASE_URL
+echo $DATABASE_URL
+tsh init-db $DATABASE_URL
+tsh formula-init-db $DATABASE_URL
+rework init-db $DATABASE_URL
 tsh register-tasks
