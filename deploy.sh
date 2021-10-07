d0359c9f0e5701659e870d744ff07c86538d8d52
qx6145 <loic.balland@engie.com>
2021-10-07 22:16:14 +0200

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-echo $DATABASE_URL
-tsh init-db $DATABASE_URL
-tsh formula-init-db $DATABASE_URL
-rework init-db $DATABASE_URL
+PROPER="${$DATABASE_URL//postgres/postgresql}"
+echo "heroku uri: $DATABASE_URL; proper heroku uri: $PROPER"
+tsh init-db $PROPER
+tsh formula-init-db $PROPER
+rework init-db $PROPER
 tsh register-tasks
