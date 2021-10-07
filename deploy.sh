b7afd710cc6e4d7c1d858c51f1e0e149c9186232
qx6145 <loic.balland@engie.com>
2021-10-07 19:45:59 +0200

    Add template rework task

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 export INSTALL_ON_LINUX=1;pip install -e.
 tsh init-db DATABASE_URL --no-dry-run
+rework init-db DATABASE_URL --no-dry-run
--- open_saturn/cli.py
+++ open_saturn/cli.py
@@ -1,7 +1,33 @@
 import click
-from open_saturn.helper import host
+
+from time import sleep
+from rework import api
+from sqlalchemy import create_engine
+
+from open_saturn.helper import host, config
 
 @click.command()
 def openwebstart():
     from open_saturn.wsgi import app
     app.run(debug=True)
+
+@saturnoil.command('register-tasks')
+@click.option('--cleanup', is_flag=True, default=False)
+def register_tasks():
+    dburi = config()['db']['uri']
+    engine = create_engine(dburi)
+
+    @api.task
+    def my_first_task(task):
+        with task.capturelogs(std=True):
+            print('I am running')
+            somevalue = task.input * 2
+            task.save_output(somevalue)
+            print('I am done')
+
+    if cleanup:
+        with engine.begin() as cn:
+            cn.execute(
+                "delete from rework.operation where path like '%%open_saturn%%'")
+
+    api.freeze_operations(engine)
