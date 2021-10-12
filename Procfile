f4ffd999dd3150f38615010a7987e74543fc3976
qx6145 <loic.balland@engie.com>
2021-10-12 11:30:22 +0200

    add rework monitor worker

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: bash deploy.sh
+worker: rework monitor --maxruns 1 --maxworkers 6 --minworkers 0
 web: gunicorn open_saturn.wsgi:app
--- deploy.py
+++ deploy.py
@@ -8,6 +8,7 @@ def replace_uri(refinery_file):
     if 'DATABASE_URL' in os.environ.keys() is not None:
         prefix = 'postgres'
         url = os.environ['DATABASE_URL'].replace(prefix, prefix + 'ql')
+        os.environ['REWORKCFGPATH'] = url
         if not config.get('db', 'uri').startswith('postgresql'):
             config.set('db', 'uri', url)
     with open(refinery_file, 'w') as configfile:
