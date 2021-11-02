a48cbf2c4c81d386d9e99bf5cad391d24e680cd3
lofriedman <loic.balland@engie.com>
2021-11-02 19:34:18 +0100

    egg

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def hgsr_repo(repo_name, version):
 
 _REQUIREMENTS = [
     'flask<2.0',
-    'flask-oidc @ https://github.com/puiterwijk/flask-oidc/archive/master.tar.gz#egg=flask-oidc',
+    'flask-oidc @ https://github.com/puiterwijk/flask-oidc/archive/master.zip',
     'okta',
     'pandas<1.2',
     'mock',
