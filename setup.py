06182b0febd5e6ffb43ba52757e124cad3c6026a
lofriedman <loic.balland@engie.com>
2021-11-02 18:43:24 +0100

    typo

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def hgsr_repo(repo_name, version):
 
 _REQUIREMENTS = [
     'flask<2.0',
-    'flask-oidc @ https://github.com/puiterwijk/flask-oidc#egg=flask-oidc',
+    'flask-oidc @ https://github.com/puiterwijk/flask-oidc.git#egg=flask-oidc',
     'okta',
     'pandas<1.2',
     'mock',
