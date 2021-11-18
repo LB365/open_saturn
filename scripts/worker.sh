1be4b50f84930fb3c8cde9e4d11594d52d7f56c8
lofriedman <loic.balland@engie.com>
2021-11-18 10:55:56 +0100

    rm finished arg

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,3 +1,3 @@
 PROPER="${DATABASE_URL//postgres/postgresql}"
-rework vacuum $PROPER --workers --tasks --finished
+rework vacuum $PROPER --workers --tasks
 rework monitor $PROPER --maxruns 1 --maxworkers 3 --minworkers 0
