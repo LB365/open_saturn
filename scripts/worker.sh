29c69633a417aa570b3187b67edecd9b03e31387
lofriedman <loic.balland@engie.com>
2021-11-18 10:55:32 +0100

    cleanup workers

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,3 +1,3 @@
 PROPER="${DATABASE_URL//postgres/postgresql}"
 rework vacuum $PROPER --workers --tasks --finished
-rework monitor $PROPER --maxworkers 3
+rework monitor $PROPER --maxruns 1 --maxworkers 3 --minworkers 0
