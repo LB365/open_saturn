a6c1087d37259853fca00e740e5d3e86894c5e1f
lofriedman <loic.balland@engie.com>
2021-11-20 13:49:37 +0100

    kill workers that stay

---
+++ scripts/kill-workers.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from open_saturn.helper import config
+
+
+def kill_workers():
+    """ kills all workers
+    """
+    engine = create_engine(config()['db']['uri'])
+    with engine.begin() as cn:
+        update('rework.worker').values(
+            kill=True
+        ).do(cn)
+
+
+if __name__ == '__main__':
+    kill_workers()
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,2 +1,3 @@
 PROPER="${DATABASE_URL//postgres/postgresql}"
-rework monitor $PROPER --maxruns 1 --maxworkers 1 --minworkers 0
+python ./scripts/kill-workers.py
+rework monitor $PROPER --maxworkers 6
