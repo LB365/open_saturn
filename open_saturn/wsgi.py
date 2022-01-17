5677d63edc03bc3bbc426611f58cf7bef0c979a6
lofriedman <loic.balland@engie.com>
2022-01-17 16:51:44 +0100

    print conf

--- open_saturn/wsgi.py
+++ open_saturn/wsgi.py
@@ -3,10 +3,14 @@ from tshistory_refinery.wsgi import ReverseProxied
 from open_saturn.helper import config
 from open_saturn.webapi import make_okta_app, make_open_app
 
-app_type = {
+types = {
     'dev': make_open_app,
     'prd': make_okta_app,
 }
 
-app = app_type[os.environ['APP_TYPE']](config())
+conf = config()
+print(conf)
+app_type = os.environ['APP_TYPE']
+app = types[app_type](conf)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.wsgi_app = ReverseProxied(app.wsgi_app)
