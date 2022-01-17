6e1c0048096a7809e8c787a57de267e7ad84a7c9
lofriedman <loic.balland@engie.com>
2022-01-17 23:00:20 +0100

    back to 3.8

--- open_saturn/wsgi.py
+++ open_saturn/wsgi.py
@@ -1,12 +1,14 @@
 import os
 from tshistory_refinery.wsgi import app
 
+from open_saturn.helper import config
 from open_saturn.webapi import make_okta_app, make_open_app
 
 types = {
     'dev': make_open_app,
     'prd': make_okta_app,
 }
-
+config_ = config()
+print(config_.__dict__)
 app_type = os.environ['APP_TYPE']
 app = types[app_type](app)
