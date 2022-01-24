dcb8aa672fe564e036fd8aac962114edd06fed1a
lballand-ea <loic.balland@energyaspects.com>
2022-01-24 20:42:25 +0100

    print token

--- open_saturn/dashboard.py
+++ open_saturn/dashboard.py
@@ -45,7 +45,7 @@ def _single_graph(tsa:timeseries, title:str, plots:PlotConfig):
     charts = dw.get_charts(search=title)
     args = data, PLOTS, title, None
     if not charts:
-        create_single_plot(*args[1:])
+        create_single_plot(data=None, config=PLOTS, title=title, token=None)
     return update_single_plot(*args)
     
 
